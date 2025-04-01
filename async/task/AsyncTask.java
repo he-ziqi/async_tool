67b80ae9e98256892273c1be82c690d788800d95
he-ziqi <62747607+he-ziqi@users.noreply.github.com>
2025-04-01 13:02:51 +0800

    Update AsyncTask.java

--- async/task/AsyncTask.java
+++ async/task/AsyncTask.java
@@ -43,21 +43,15 @@ public class AsyncTask<R> implements CallableTask{
     private volatile boolean completed = false;
 
     public AsyncTask(String taskName, Supplier<R> task) {
-        this.taskName = taskName;
-        this.task = task;
+        this(taskName,task,DEFAULT_TIMEUNIT,DEFAULT_TIMEOUT);
     }
 
     public AsyncTask(String taskName, Supplier<R> task,TimeUnit timeUnit,long timeout) {
-        this.taskName = taskName;
-        this.task = task;
-        this.timeout = timeout;
-        this.timeUnit = timeUnit;
+        this(taskName, task,null,timeUnit,timeout);
     }
 
     public AsyncTask(String taskName, Supplier<R> task, Function<? super Throwable, ? super R> exHandler) {
-        this.taskName = taskName;
-        this.task = task;
-        this.exHandler = exHandler;
+        this(taskName,task,exHandler,DEFAULT_TIMEUNIT,DEFAULT_TIMEOUT);
     }
 
     public AsyncTask(String taskName, Supplier<R> task, Function<? super Throwable, ? super R> exHandler,TimeUnit timeUnit,long timeout) {
@@ -69,22 +63,9 @@ public class AsyncTask<R> implements CallableTask{
     }
 
     @Override
-    public long getTimeout() {
-        return timeout;
-    }
-
-    @Override
-    public TimeUnit getTimeUnit() {
-        return timeUnit;
-    }
-
-    @Override
     public <T> void setResult(T result) {
         this.result = (R) result;
     }
 
-    @Override
-    public boolean isComplete() {
-        return completed;
-    }
 }
+
